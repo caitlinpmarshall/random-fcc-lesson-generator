b4de82932b7744cfad62080ec7577c9029a0a718
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-08-23 13:03:06 -0400

    Combines random number generator into lesson link component.  Random number works and renders properly, lesson link does not

--- src/LessonLink.js
+++ src/LessonLink.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 //randomizer should take a random number, within the length of the data model array, and choose THAT as the index of what to display
 
-const LessonLink = (props) => (
-  <div className="">
-    <h3 className="title">Your randomly selected lesson of the day is:</h3>
-    <h2 className="name">{props.link}</h2>
-    <button>This isn't clickable yet. Refresh.</button>
-  </div>
-);
+class LessonLink extends Component {
+  state = { value: 0 };
+  getRandomNumber = () => Math.floor(Math.random() * this.props.max);
+  handleClick = () => {
+    this.setState({ value: this.getRandomNumber() });
+  };
+  render() {
+    return (
+      <>
+        <div className="App">
+          <h1>{this.state.value}</h1>
+          <button onClick={this.handleClick}>Get Random Number</button>
+        </div>
+        <div className="link">
+          <h3 className="title">
+            Your randomly selected lesson of the day is:
+          </h3>
+          {/* <h2 className="name">{props.link}</h2> */}
+          <button>This isn't clickable yet. Refresh.</button>
+        </div>
+      </>
+    );
+  }
+}
 
 export default LessonLink;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import RandomApp from "./RandomApp.js";
 import { learnBasicJS } from "./data.js";
 
 //tried putting filterId here, but understandably, it's asking, what props? How do I know to go look at RandomApp?
-const filterId = (lesson) => (lesson.id = props.state.value);
+//const filterId = (lesson) => (lesson.id = props.state.value);
 
 const renderLessonLink = (lesson) => (
   <LessonLink link={lesson.link} key={lesson.id} />
@@ -18,14 +18,12 @@ const renderLessonLink = (lesson) => (
 
 const App = () => {
   const lessonLinkElement = learnBasicJS.map(renderLessonLink);
-  const randomId = learnBasicJS.filter(filterId);
 
   return (
     <div className="App">
       <h1 className="header">FreeCodeCamp Randomizer</h1>
       <RandomApp max={learnBasicJS.length} />
       {lessonLinkElement}
-      {randomId}
       {/*<Confetti />*/}
     </div>
   );
